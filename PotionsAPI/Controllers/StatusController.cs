8b3aeaacebf4a3dd51e2d60ad2a380645a1f31b6
Peedroca <phm1080@gmail.com>
2020-08-26 17:37:51 -0300

    API Created

---
+++ PotionsAPI/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PotionsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("It's works");
+        }
+    }
+}
